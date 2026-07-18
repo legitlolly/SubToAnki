4ea764a095308bd6cfd612f8893a5df50074e165
legitlolly <jmatthews17754@gmail.com>
2026-07-18 22:44:11 +0100

    Working example from single entry - improvements made to kanji struct for frequency feature

--- buildDb.go
+++ buildDb.go
@@ -1,12 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"os"
+)
+
 type Entry struct {
-	Seq      int      `xml:"ent_deq"`
-	Kanji    []string `xml:"k_ele>keb"`
+	Seq      int      `xml:"ent_seq"`
+	Kanji    []Kanji  `xml:"k_ele"`
 	Readings []string `xml:"r_ele>reb"`
 	Senses   []Sense  `xml:"sense"`
+}
+type Kanji struct {
+	Text      string   `xml:"keb"`
+	Frequency []string `xml:"ke_pri"` // How often its seen this is defined as
+	Info      string   `xml:"ke_inf"`
 }
 type Sense struct {
 	Pos     []string `xml:"pos"`  // There are multiple possible poses v5r & vi == godan and intransitive
 	Xrefs   []string `xml:"xref"` // References
-	Ants    []string `xml:"ant"`
+	Ants    []string `xml:"ant"`  // Words opposite
 	Glosses []string `xml:"gloss"`
+}
+
+func main() {
+	data, err := os.ReadFile("example.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dec := xml.NewDecoder(bytes.NewReader(data))
+	dec.Strict = false
+
+	var e Entry
+	if err := dec.Decode(&e); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", e)
 }
