1e7cd55b815499b016a8d2132a4a33a0f9422d26
legitlolly <jmatthews17754@gmail.com>
2026-07-19 17:06:27 +0100

    Now can lookup words and get their ids from the database - TODO - LIst readings.

--- cmd/subtoanki/main.go
+++ cmd/subtoanki/main.go
@@ -21,7 +21,7 @@ func main() {
 		if len(os.Args) < 3 {
 			log.Fatal("usage: subtoanki lookup <word_to_lookup>")
 		}
-		if err := jmdict.Lookup(); err != nil {
+		if err := jmdict.Lookup(os.Args[2]); err != nil {
 			log.Fatal(err)
 		}
 	default:
--- internal/jmdict/lookup.go
+++ internal/jmdict/lookup.go
@@ -1,5 +1,53 @@
 package jmdict
 
-func Lookup() error {
+import (
+	"database/sql"
+	"fmt"
+)
+
+func Lookup(word string) error {
+	db, err := sql.Open("sqlite", "lookup.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`
+	SELECT entry_id FROM kanji WHERE text = ?
+	UNION
+	SELECT entry_id FROM readings WHERE text = ?
+	`, word, word)
+	if err != nil {
+		return err
+	}
+	var ids []int
+	for rows.Next() {
+		var id int
+		if err := rows.Scan(&id); err != nil {
+			rows.Close()
+			return err
+		}
+		ids = append(ids, id)
+	}
+	rows.Close()
+	if len(ids) == 0 {
+		fmt.Println("not found")
+		return nil
+	}
+	for _, id := range ids {
+		rows, err := db.Query(`SELECT text, info FROM kanji WHERE entry_id = ?`, id)
+		if err != nil {
+			return err
+		}
+		for rows.Next() {
+			var text, info string
+			if err := rows.Scan(&text, &info); err != nil {
+				rows.Close()
+				return err
+			}
+			fmt.Println(id, text, info)
+		}
+		rows.Close()
+	}
 	return nil
 }
